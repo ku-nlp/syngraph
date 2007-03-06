75f90cb87c429590191de54878935e9ff0ee7882
Mitachika Odani <odani@(none)>
2007-03-06 09:54:07 +0000

    program_fixed

--- scripts/knp_syn.pl
+++ scripts/knp_syn.pl
@@ -14,14 +14,11 @@ binmode STDOUT, ':encoding(euc-jp)';
 binmode STDERR, ':encoding(euc-jp)';
 binmode DB::OUT, ':encoding(euc-jp)';
 
-my %opt; GetOptions(\%opt, 'debug', 'postprocess', 'relation', 'antonym');
+my %opt; GetOptions(\%opt, 'sentence=s', 'debug', 'postprocess', 'relation', 'antonym');
 
 # my $sentence = '一番近い駅';
- my $sentence = '彼の歩き方を学ぶ';
+ my $input = '彼の歩き方を学ぶ';
 #my $sentence = '彼は彼女を助ける';
-if ($ARGV[0]) {
-    $sentence = decode('euc-jp', $ARGV[0]);
-}
 
 my $option;
 my $knp_option;
@@ -36,27 +33,54 @@ my $SynGraph = new SynGraph($knp_option);
 # 類義表現DBをtie
 $SynGraph->tie_syndb('../syndb/syndata.mldbm', '../syndb/synhead.mldbm', '../syndb/synparent.mldbm', '../syndb/synantonym.mldbm');
 
-# SynGraphを作成
-my $syngraph = {};
-$syngraph->{parse} = $SynGraph->{knp}->parse($sentence);
-#$syngraph->{parse}->set_id('syngraph');
-$syngraph->{graph} = {};
-$SynGraph->make_sg($syngraph->{parse}, $syngraph->{graph}, $syngraph->{parse}->id, $regnode_option);
-Dumpvalue->new->dumpValue($syngraph) if ($option->{debug});
+if ($opt{sentence}) {
+    $input = decode('euc-jp', $opt{sentence});
+    my $result = $SynGraph->{knp}->parse($input);
+    &outputformat($result);
+}
+else {
+    my ($sid, $knp_buf);
+    while (<>) {
+	$knp_buf .= $_;
 
-# SynGraphをformat化
-$syngraph->{format} = $SynGraph->format_syngraph($syngraph->{graph}->{$syngraph->{parse}->id});
-
-# KNPと併せて出力
-print $syngraph->{parse}->comment;
-my $bp = 0;
-foreach ($syngraph->{parse}->bnst) {
-    print "* ", defined $_->{parent} ? $_->{parent}->id : '-1', "$_->{dpndtype} $_->{fstring}\n";
-    foreach ($_->tag) {
-	printf $syngraph->{format}->[$bp];
-	printf $_->spec;
-	$bp++;
+	if (/^EOS$/) {
+	    my $result = new KNP::Result($knp_buf);
+	    $result->set_id($sid) if ($sid);
+	    &outputformat($result);
+	}
+	elsif (/\# S-ID:(.+) KNP:/) {
+	    $sid = $1;
+	    $sid =~ s/\s+/ /;
+	    $sid =~ s/^\s//;
+	    $sid =~ s/\s$//;
+	}
     }
 }
 
-print "EOS\n";
+sub outputformat { 
+    my ($result) = @_;
+
+    my $syngraph = {};
+
+    $syngraph->{parse} = $result;
+    $syngraph->{graph} = {};
+    $SynGraph->make_sg($syngraph->{parse}, $syngraph->{graph}, $syngraph->{parse}->id, $regnode_option);
+    Dumpvalue->new->dumpValue($syngraph) if ($option->{debug});
+
+# SynGraphをformat化
+    $syngraph->{format} = $SynGraph->format_syngraph($syngraph->{graph}->{$syngraph->{parse}->id});
+
+# KNPと併せて出力
+    print $syngraph->{parse}->comment;
+    my $bp = 0;
+    foreach ($syngraph->{parse}->bnst) {
+	print "* ", defined $_->{parent} ? $_->{parent}->id : '-1', "$_->{dpndtype} $_->{fstring}\n";
+	foreach ($_->tag) {
+	    printf $syngraph->{format}->[$bp];
+	    printf $_->spec;
+	    $bp++;
+	}
+    }
+
+    print "EOS\n";
+}
