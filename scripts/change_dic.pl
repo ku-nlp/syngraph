33d72cf8760c84fb9b7ed120a22ce79a23ebf4af
Mitachika Odani <odani@(none)>
2007-04-15 13:54:48 +0000

    add

---
+++ scripts/change_dic.pl
@@ -0,0 +1,222 @@
+#!/usr/local/bin/perl
+
+use strict;
+use Dumpvalue;
+use Getopt::Long;
+use encoding 'euc-jp'; # ソースコード中の文字列にeucフラグを付与する。ソースはeucで書くこと！！（C-x Return f）
+binmode STDERR, ':encoding(euc-jp)';
+binmode DB::OUT, ':encoding(euc-jp)';
+
+my %wordid;
+
+my %opt; GetOptions(\%opt, 'synonym=s', 'definition=s', 'relation=s', 'antonym=s', 'synonym_change=s', 'relation_change=s', 'antonym_change=s');
+
+open (FILE, '<:encoding(euc-jp)', $opt{definition}) || die; #  PerlIOレイヤを指定する。上で宣言したレイヤは使用されない。
+while (<FILE>) {
+
+    my $sent = $_;
+    my @words = split (/\s/, $sent);
+    
+    foreach my $word (@words) {
+	if ($word =~ /:/) {
+	    my $flag;
+	    foreach (@{$wordid{(split(/:/, $word))[0]}}) {
+		if ($_ eq $word) {
+		    $flag = 1;
+		    last;
+		}
+	    }
+	    next if ($flag);
+	    push(@{$wordid{(split(/:/, $word))[0]}}, $word);
+	}
+    }
+}
+close(FILE);
+
+open (FILE, '<:encoding(euc-jp)', $opt{synonym}) || die; #  PerlIOレイヤを指定する。上で宣言したレイヤは使用されない。
+while (<FILE>) {
+
+    my $sent = $_;
+    my @words = split (/\s/, $sent);
+    
+    foreach my $word (@words) {
+	if ($word =~ /:/) {
+	    my $flag;
+	    foreach (@{$wordid{(split(/:/, $word))[0]}}) {
+		if ($_ eq $word) {
+		    $flag = 1;
+		    last;
+		}
+	    }
+	    next if ($flag);
+	    push(@{$wordid{(split(/:/, $word))[0]}}, $word);
+	}
+    }
+}
+close(FILE);
+
+open (FILE, '<:encoding(euc-jp)', $opt{relation}) || die; #  PerlIOレイヤを指定する。上で宣言したレイヤは使用されない。
+while (<FILE>) {
+
+    my $sent = $_;
+    my @words = split (/\s/, $sent);
+    
+    foreach my $word (@words) {
+	if ($word =~ /:/) {
+	    my $flag;
+	    foreach (@{$wordid{(split(/:/, $word))[0]}}) {
+		if ($_ eq $word) {
+		    $flag = 1;
+		    last;
+		}
+	    }
+	    next if ($flag);	    
+	    push(@{$wordid{(split(/:/, $word))[0]}}, $word);
+	}
+    }
+}
+close(FILE);
+
+open (FILE, '<:encoding(euc-jp)', $opt{antonym}) || die; #  PerlIOレイヤを指定する。上で宣言したレイヤは使用されない。
+while (<FILE>) {
+
+    my $sent = $_;
+    my @words = split (/\s/, $sent);
+    
+    foreach my $word (@words) {
+	if ($word =~ /:/) {
+	    my $flag;
+	    foreach (@{$wordid{(split(/:/, $word))[0]}}) {
+		if ($_ eq $word) {
+		    $flag = 1;
+		    last;
+		}
+	    }
+	    next if ($flag);
+	    
+	    push(@{$wordid{(split(/:/, $word))[0]}}, $word);
+	}
+    }
+}
+close(FILE);
+
+#Dumpvalue->new->dumpValue(\%wordid);
+
+open (FILE, '<:encoding(euc-jp)', $opt{synonym}) || die; #  PerlIOレイヤを指定する。上で宣言したレイヤは使用されない。
+open(CHANGE, '>:encoding(euc-jp)', $opt{synonym_change}) or die;
+while (<FILE>) {
+
+    my $sent = $_;
+    my @words = split (/\s/, $sent);
+    
+    foreach my $word (@words) {
+	if ($word !=~ /:/) {
+	    if ($wordid{$word}) {
+		foreach (@{$wordid{$word}}) {
+		    print CHANGE "$_ ";			
+		}
+	    }
+	    else {
+		print CHANGE "$word ";			
+	    }
+	}
+	else{
+	    print CHANGE "$word ";	
+	}
+    }
+    print CHANGE "\n";
+}
+close(FILE);
+close(CHANGE);
+
+open (FILE, '<:encoding(euc-jp)', $opt{relation}) || die; #  PerlIOレイヤを指定する。上で宣言したレイヤは使用されない。
+open(CHANGE, '>:encoding(euc-jp)', $opt{relation_change}) or die;
+while (<FILE>) {
+
+    my $sent = $_;
+    my @words = split (/\s/, $sent);
+    my @word0_list;
+    my @word1_list;
+    
+    if ($words[0] !=~ /:/) {
+	if ($wordid{$words[0]}) {
+	    foreach (@{$wordid{$words[0]}}) {
+		push @word0_list, $_;
+		}
+	}
+	else {
+	    push @word0_list, $words[0];
+	    }
+    }
+    else{
+	push @word0_list, $words[0];
+    }
+
+    if ($words[1] !=~ /:/) {
+	if ($wordid{$words[1]}) {
+	    foreach (@{$wordid{$words[1]}}) {
+		push @word1_list, $_;
+		}
+	}
+	else {
+	    push @word1_list, $words[1];
+	    }
+    }
+    else{
+	push @word1_list, $words[1];
+    }
+
+    foreach my $wordid0 (@word0_list) {
+	foreach my $wordid1 (@word1_list) {
+	    print CHANGE "$wordid0 $wordid1\n";
+	}
+    }
+}
+close(FILE);
+close(CHANGE);
+
+open (FILE, '<:encoding(euc-jp)', $opt{antonym}) || die; #  PerlIOレイヤを指定する。上で宣言したレイヤは使用されない。
+open(CHANGE, '>:encoding(euc-jp)', $opt{antonym_change}) or die;
+while (<FILE>) {
+
+    my $sent = $_;
+    my @words = split (/\s/, $sent);
+    my @word0_list;
+    my @word1_list;
+
+    if ($words[0] !=~ /:/) {
+	if ($wordid{$words[0]}) {
+	    foreach (@{$wordid{$words[0]}}) {
+		push @word0_list, $_;
+		}
+	}
+	else {
+	    push @word0_list, $words[0];
+	    }
+    }
+    else{
+	push @word0_list, $words[0];
+    }
+
+    if ($words[1] !=~ /:/) {
+	if ($wordid{$words[1]}) {
+	    foreach (@{$wordid{$words[1]}}) {
+		push @word1_list, $_;
+		}
+	}
+	else {
+	    push @word1_list, $words[1];
+	    }
+    }
+    else{
+	push @word1_list, $words[1];
+    }
+
+    foreach my $wordid0 (@word0_list) {
+	foreach my $wordid1 (@word1_list) {
+	    print CHANGE "$wordid0 $wordid1\n";
+	}
+    }
+}
+close(FILE);
+close(CHANGE);
