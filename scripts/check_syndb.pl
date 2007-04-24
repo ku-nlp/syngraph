0631b8501dd37d69458b69cd8d1d24b44b06870c
Mitachika Odani <odani@(none)>
2007-04-24 07:19:27 +0000

    DBチェック関数でそのSYNIDの上位、反義のSYNID表示

--- scripts/check_syndb.pl
+++ scripts/check_syndb.pl
@@ -37,6 +37,16 @@ print "# S-ID:$synid\n";
 my $result = $SynGraph->{syndb}->{$synid};
 $result =~ s/\|/  \|  /g;
 print $result, "\n";
+if (defined $SynGraph->{synparent}->{$synid}) {
+    foreach my $pid (keys %{$SynGraph->{synparent}->{$synid}}) {
+	print "上位：$pid\n";
+    }
+}
+if (defined $SynGraph->{synantonym}->{$synid}) {
+    foreach my $aid (keys %{$SynGraph->{synantonym}->{$synid}}) {
+	print "反義：$aid\n";
+    }
+}
 
 if ($opt{print_syngraph}) {
     # 同義グループに所属するSYNGRAPHを出力
