fcad7e7bc2136ae40418aff3b2bf79e70bd92fbb
Mitachika Odani <odani@(none)>
2007-07-09 05:50:26 +0000

    同義グループをマージする手順を追加

--- scripts/merge_dic.sh
+++ scripts/merge_dic.sh
@@ -7,6 +7,9 @@ SRC_DIR='.'
 SIM_DIR_Dic=../dic/rsk_iwanami
 SIM_DIR_Web=../dic/web_news
 
+# 同義表現データマージ途中のディレクトリ
+SIM_M_DIR=../dic_middle
+
 # 同義表現データマージ後のディレクトリ
 SIM_C_DIR=../dic_change
 
@@ -48,10 +51,20 @@ echo "STEP1 start\t`date`"
 # 辞書からの知識抽出のログ作成（CGI用）
 perl -I$PERL_DIR make_logdic.pl --synonym=$SIM_DIR_Dic/synonym.txt --definition=$SIM_DIR_Dic/definition.txt --isa=$SIM_DIR_Dic/isa.txt --antonym=$SIM_DIR_Dic/antonym.txt --syndbdir=$SYNDB_DIR
 
+# 辞書からの同義関係(synonym, same_difinition)の知識の連結
+perl cat_synonym_same_def.pl -synonym_dic=$SIM_DIR_Dic/synonym.txt -same_diff=$SIM_DIR_Dic/same_definition.txt -cat_file=$SIM_M_DIR/synonym_dic.txt
+
+# 辞書からの知識の整理
+perl check_synonym_dic.pl --synonym_dic=$SIM_M_DIR/synonym_dic.txt --log_merge=$SIM_C_DIR/log_merge_synonym_dic.txt --change=$SIM_M_DIR/synonym_dic2.txt
+perl check_synonym_dic.pl --synonym_dic=$SIM_DIR_Dic/antonym.txt --log_merge=$SIM_C_DIR/log_merge_antonym_dic.txt --change=$SIM_M_DIR/antonym.txt
+
+# Webからの知識の整理
+#perl check_duplicate_entry.pl -rnsame --synonym_web_news=$SIM_DIR_Web/all.txt --log_merge=$SIM_C_DIR/log_merge_synonym_web_news.txt --change=$SIM_M_DIR/synonym_web_news.txt
+
+# Webからの知識から辞書からの知識と重複を削除する
+perl check_dic_web_news_duplicate.pl --dic=$SIM_M_DIR/synonym_dic2.txt --web=$SIM_M_DIR/synonym_web_news.txt --log_merge=$SIM_C_DIR/log_delete_web_news.txt --change=$SIM_C_DIR/synonym_web_news.txt
+
 # 辞書を変換（多義性の扱い）
-perl -I$PERL_DIR change_dic.pl --synonym=$SIM_DIR_Dic/synonym.txt --definition=$SIM_DIR_Dic/definition.txt --isa=$SIM_DIR_Dic/isa.txt --antonym=$SIM_DIR_Dic/antonym.txt --synonym_change=$SIM_C_DIR/synonym_dic.txt --isa_change=$SIM_C_DIR/isa.txt --antonym_change=$SIM_C_DIR/antonym.txt
+perl -I$PERL_DIR change_dic.pl --synonym=$SIM_M_DIR/synonym_dic2.txt --definition=$SIM_DIR_Dic/definition.txt --isa=$SIM_DIR_Dic/isa.txt --antonym=$SIM_M_DIR/antonym.txt --synonym_change=$SIM_C_DIR/synonym_dic.txt --isa_change=$SIM_C_DIR/isa.txt --antonym_change=$SIM_C_DIR/antonym.txt
 
 cp $SIM_DIR_Dic/definition.txt $SIM_C_DIR/definition.txt
-
-# Webからの辞書の整理
-perl check_duplicate_entry.pl -rnsame --synonym_web_news=$SIM_DIR_Web/all.txt --log_merge=$SIM_C_DIR/log_merge.txt --change=$SIM_C_DIR/synonym_web_news.txt
