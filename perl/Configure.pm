e74ca5d6d372b188ec06e047c84ba7a3416352db
Tomohide Shibata <shibata@i.kyoto-u.ac.jp>
2007-08-31 09:23:17 +0000

    added.

---
+++ perl/Configure.pm
@@ -0,0 +1,15 @@
+package Constant;
+
+# $Id$
+
+# 定数をこのファイルで定義
+
+use strict;
+use utf8;
+
+our $JumanCommand = '/home/harashima/usr/bin/juman';
+our $JumanRcfile = '/home/harashima/usr/etc/jumanrc';
+
+our $CalcsimMidbfile = '/home/shibata/work/CalcSimilarityByCF/db/all-mi';
+
+1;
