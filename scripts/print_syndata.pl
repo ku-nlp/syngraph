7a1e6e566dab74750587fb130fdeb196211b9075
Tomohide Shibata <shibata@i.kyoto-u.ac.jp>
2007-09-04 06:54:20 +0000

    added.

---
+++ scripts/print_syndata.pl
@@ -0,0 +1,38 @@
+#!/usr/bin/env perl
+
+# syndataをprintするスクリプト
+
+# usage: perl -I../perl print_syndata.pl ../syndb/i686/syndata.mldbm
+
+use strict;
+use SynGraph;
+use utf8;
+binmode STDIN, ':encoding(euc-jp)';
+binmode STDOUT, ':encoding(euc-jp)';
+
+my $syndata_mldbm = $ARGV[0];
+
+my %db;
+
+&SynGraph::tie_mldbm($syndata_mldbm, \%db);
+
+# 出力例
+
+# s10000:オープンキャンパス,オープンキャンパス
+#  オープン/おーぷん|オープンだ/おーぷんだ|s1703:オープン/おーぷん|s35492:オープン/おーぷん
+#  キャンパス/きゃんぱす|s31654:キャンパス/きゃんぱす
+
+foreach my $key ( sort { $a cmp $b } keys %db) {
+    print $key, "\n";
+
+    # 基本句
+    foreach my $bp (@{$db{$key}}) {
+	my @ids;
+
+	# 基本ノードとSynノード
+	foreach my $node (@{$bp}) {
+	    push @ids, $node->{id};
+	}
+	print ' ', join('|', @ids), "\n";
+    }
+}
