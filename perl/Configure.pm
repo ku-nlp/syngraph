e22953213a6360911a58050eafc205aa6b991dbc
Tomohide Shibata <shibata@i.kyoto-u.ac.jp>
2007-09-11 06:12:23 +0000

    $KnpCommandなどを追加

--- perl/Configure.pm
+++ perl/Configure.pm
@@ -7,9 +7,20 @@ package Constant;
 use strict;
 use utf8;
 
+our $SynGraphBaseDir = '/home/shibata/work/SynGraph';
+
 our $JumanCommand = '/home/harashima/usr/bin/juman';
 our $JumanRcfile = '/home/harashima/usr/etc/jumanrc';
+our $KnpCommand = '/home/shibata/tool/bin/knp';
+our $KnpRcfile = '/home/shibata/.knprc';
 
+our $JumanShareCommand = '/share/usr/bin/juman';
+our $KnpShareCommand = '/share/usr/bin/knp';
+
+# for CGI
+our $RSK4CGIdb = '/home/harashima/tools/dictionary/rsk/rsk4cgi.db';
+
+# 分布類似度計算用のデータベース
 our $CalcsimMidbfile = '/home/shibata/work/CalcSimilarityByCF/db/all-mi';
 
 1;
