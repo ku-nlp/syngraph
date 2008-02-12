88054b16ec1e8025fc9b2292d3a2ee4d0c65c14d
Tomohide Shibata <shibata@i.kyoto-u.ac.jp>
2008-02-12 15:05:08 +0000

    added.

---
+++ scripts/extract_han_includes_disambiguate_hiragana.pl
@@ -0,0 +1,59 @@
+#!/usr/bin/env perl
+
+# 曖昧性のあるひらがなの代表表記を含む文を抽出する
+
+# usage: echo '蕎麦を食べた' | juman | perl extract_han_includes_disambiguate_hiragana.pl --db ../db/hiragana_disambiguate.cdb 
+
+use utf8;
+binmode STDIN, ':encoding(euc-jp)';
+binmode STDOUT, ':encoding(euc-jp)';
+binmode STDERR, ':encoding(euc-jp)';
+binmode DB::OUT, ':encoding(euc-jp)';
+use Encode;
+use Getopt::Long;
+use CDB_File;
+use Juman;
+
+my (%opt);
+GetOptions(\%opt, 'db=s', 'debug');
+
+my %hiragana2han;
+my $db =  tie %hiragana2han, 'CDB_File', "$opt{db}" or die;
+
+my %han2hiragana;
+
+foreach my $hiragana (keys %hiragana2han) {
+    $hiragana = decode('utf8', $hiragana);
+    foreach my $han (split('\?', decode('utf8', $hiragana2han{$hiragana}))) {
+	$han2hiragana{$han} = $hiragana;
+    }
+}
+
+while (<>) {
+    $buf .= $_;
+
+    if (/EOS/) {
+	my $result = new Juman::Result($buf);
+	my $sentence = &get_sentence($result);
+	foreach my $mrph ($result->mrph) {
+	    my $repname = $mrph->repname;
+
+	    if ($repname && defined $han2hiragana{$repname}) {
+		print "$han2hiragana{$repname} $repname $sentence\n";
+	    }
+	}
+	$buf = '';
+    }
+}
+
+sub get_sentence {
+    my ($result) = @_;
+
+    my $sentence;
+    foreach my $mrph ($result->mrph) {
+	$sentence .= $mrph->midasi;
+    }
+    return $sentence;
+}
+
+untie %hiragana2han;
