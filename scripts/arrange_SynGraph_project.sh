0bbb4db87b90defcce5f2b4dc57142f09f8251cf
Tomohide Shibata <shibata@i.kyoto-u.ac.jp>
2008-11-21 05:08:44 +0000

    added.

---
+++ scripts/arrange_SynGraph_project.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env zsh
+
+# 不必要なスクリプト、データを削除
+
+# usage: cd scripts; ./arrange_SynGraph_project.sh
+
+rm -fr ../syndb/x86_64
+rm -fr ../syndb/cgi
+rm -fr ../ExtractSynfromDic
+rm -fr ../MakeDic
