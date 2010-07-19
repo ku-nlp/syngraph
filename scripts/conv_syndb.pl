ba5116e7c1539d608f2e731f13232f1064b109a8
Tomohide Shibata <shibata@i.kyoto-u.ac.jp>
2010-07-19 15:07:29 +0000

    Medオプションを追加

--- scripts/conv_syndb.pl
+++ scripts/conv_syndb.pl
@@ -12,7 +12,7 @@ binmode STDOUT, ':encoding(euc-jp)';
 binmode STDERR, ':encoding(euc-jp)';
 binmode DB::OUT, ':encoding(euc-jp)';
 
-my %opt; GetOptions(\%opt, 'synonym_dic=s', 'synonym_web_news=s', 'definition=s', 'isa=s', 'isa_wikipedia=s', 'antonym=s', 'convert_file=s', 'syndbdir=s', 'log_merge=s', 'option=s', 'conv_log=s', 'wikipedia', 'isa_max_num=i', 'similar_phrase=s');
+my %opt; GetOptions(\%opt, 'synonym_dic=s', 'synonym_web_news=s', 'synonym_med=s', 'definition=s', 'definition_med=s', 'isa=s', 'isa_wikipedia=s', 'antonym=s', 'antonym_med=s', 'convert_file=s', 'syndbdir=s', 'log_merge=s', 'option=s', 'conv_log=s', 'wikipedia', 'isa_max_num=i', 'similar_phrase=s');
 
 # synparent.mldbm、synantonym.mldbmを置く場所
 my $dir = $opt{syndbdir} ? $opt{syndbdir} : '../syndb/i686';
@@ -38,33 +38,51 @@ my %def_delete;
 #
 # 定義文の読み込み
 #
-if ($opt{definition}) {
-    open(DEF, '<:encoding(euc-jp)', $opt{definition}) or die;
-    while (<DEF>) {
-        chomp;
-        my ($midasi, $def) = split(/ /, $_);
+my @def_file = ('definition');
+push @def_file, 'definition_med' if $opt{definition_med};
+foreach my $file_type (@def_file) {
+    if ($opt{$file_type}) {
+	open(DEF, '<:encoding(euc-jp)', $opt{$file_type}) or die;
 
-        # 定義文の例外 （★★SYNGRAPH化してからとる）
-        $def =~ s/。$//;
-        next if ($def =~ /(物ごと|…)/);
-        next if ($def =~ /の(一つ|一種)$/);
+	while (<DEF>) {
+	    chomp;
 
-        # こと・所を取る （★★SYNGRAPH化してからとる）
-        $def =~ s/のこと$//;
-        $def =~ s/こと$//;
-        $def =~ s/い所$/い/;
+	    my ($midasi, $def);
+	    if ($file_type eq 'definition_med') {
+		($midasi, $def) = split(/ /, $_, 2);
 
-        # ハッシュに登録
-	$definition{$midasi} = $def unless ($definition{$midasi});
+		$midasi = &SynGraph::h2z($midasi);
+		$def = &SynGraph::h2z($def);
+	    }
+	    else {
+		($midasi, $def) = split(/ /, $_);
+	    }
+
+
+	    # 定義文の例外 （★★SYNGRAPH化してからとる）
+	    $def =~ s/。$//;
+	    next if ($def =~ /(物ごと|…)/);
+	    next if ($def =~ /の(一つ|一種)$/);
+
+	    # こと・所を取る （★★SYNGRAPH化してからとる）
+	    $def =~ s/のこと$//;
+	    $def =~ s/こと$//;
+	    $def =~ s/い所$/い/;
+
+	    # ハッシュに登録
+	    $definition{$midasi} = $def unless ($definition{$midasi});
+	}
+
+	close(DEF);
     }
-
-    close(DEF);
 }
 
 #
 # 同義語グループの読み込み
 #
 my @file = ('synonym_dic', 'synonym_web_news');
+push @file, 'synonym_med' if $opt{synonym_med};
+
 foreach my $file_type (@file) {
     my $file_tag;
     if ($file_type eq 'synonym_dic') {
@@ -73,10 +91,15 @@ foreach my $file_type (@file) {
     elsif ($file_type eq 'synonym_web_news') {
 	$file_tag = '[Web]';
     }
+    elsif ($file_type eq 'synonym_med') {
+	$file_tag = '[Med]';
+    }
     if ($opt{$file_type}) {
 	open(SYN, '<:encoding(euc-jp)', $opt{$file_type}) or die;
 	while (<SYN>) {
 	    chomp;
+	    $_ = &SynGraph::h2z($_) if $file_type eq 'synonym_med';
+
 	    my $delimiter = ($file_type eq 'synonym_web_news' && $opt{wikipedia}) ? '\t' : '\s';
 	    my @syn_list = split(/$delimiter/, $_);
 	    
@@ -191,49 +214,57 @@ foreach my $file_type (@file) {
 #
 # 反義語の読み込み
 #
-if ($opt{antonym}) {
-    open(ANT, '<:encoding(euc-jp)', $opt{antonym}) or die;
+my @ant_file = ('antonym');
+push @file, 'antonym_med' if $opt{antonym_med};
 
-    # 矛盾解消のログ
-    if ($option{log} and $opt{log_merge}) {
-	open(LM, '>>:encoding(euc-jp)', $opt{log_merge}) or die;    
-    }
+foreach my $file_type (@ant_file) {
+    if ($opt{$file_type}) {
+	open(ANT, '<:encoding(euc-jp)', $opt{$file_type}) or die;
 
-    while (<ANT>) {
-        chomp;
-        my ($word1, $word2) = split(/ /, $_);
-
-	# SYNIDを獲得
-	my $word1syn_list = &get_synid($word1);
-	my $word2syn_list = &get_synid($word2);
-	if (&contradiction_check($word1syn_list, $word2syn_list)) {
-	    if ($option{log} and $opt{log_merge}) {
-		print LM "X contradiction isa $word1, $word2\n";
-	    }
-	    next;
+	# 矛盾解消のログ
+	if ($option{log} and $opt{log_merge}) {
+	    open(LM, '>>:encoding(euc-jp)', $opt{log_merge}) or die;    
 	}
 
-	foreach my $word1_synid (@$word1syn_list) {
-	    foreach my $word2_synid (@$word2syn_list) {
-		$antonym{$word1_synid} .= $antonym{$word1_synid} ? "|$word2_synid" : "$word2_synid" unless ($antonym{$word1_synid} =~ /$word2_synid/);
-		$antonym{$word2_synid} .= $antonym{$word2_synid} ? "|$word1_synid" : "$word1_synid" unless ($antonym{$word2_synid} =~ /$word1_synid/);
+	while (<ANT>) {
+	    chomp;
 
-		# 反義関係のログ
-		if ($option{log}) {
-		    my $key_1 = (split(/\//, $word1))[0];
-		    my $key_2 = (split(/\//, $word2))[0];
-		    $log_antonym{"$word1_synid-$word2_synid"} .= $log_antonym{"$word1_synid-$word2_synid"} ? "|$key_1-$key_2" : "$key_1-$key_2" unless $log_antonym{"$word1_synid-$word2_synid"} =~ /$key_1-$key_2/;
-		    $log_antonym{"$word2_synid-$word1_synid"} .= $log_antonym{"$word2_synid-$word1_synid"} ? "|$key_2-$key_1" : "$key_2-$key_1" unless $log_antonym{"$word2_synid-$word1_synid"} =~ /$key_2-$key_1/;
+	    my ($word1, $word2) = split(/ /, $_);
+	    $word1 = &SynGraph::h2z($word1) if $file_type eq 'antonym_med';
+	    $word2 = &SynGraph::h2z($word2) if $file_type eq 'antonym_med';
+
+	    # SYNIDを獲得
+	    my $word1syn_list = &get_synid($word1);
+	    my $word2syn_list = &get_synid($word2);
+	    if (&contradiction_check($word1syn_list, $word2syn_list)) {
+		if ($option{log} and $opt{log_merge}) {
+		    print LM "X contradiction isa $word1, $word2\n";
+		}
+		next;
+	    }
+
+	    foreach my $word1_synid (@$word1syn_list) {
+		foreach my $word2_synid (@$word2syn_list) {
+		    $antonym{$word1_synid} .= $antonym{$word1_synid} ? "|$word2_synid" : "$word2_synid" unless ($antonym{$word1_synid} =~ /$word2_synid/);
+		    $antonym{$word2_synid} .= $antonym{$word2_synid} ? "|$word1_synid" : "$word1_synid" unless ($antonym{$word2_synid} =~ /$word1_synid/);
+
+		    # 反義関係のログ
+		    if ($option{log}) {
+			my $key_1 = (split(/\//, $word1))[0];
+			my $key_2 = (split(/\//, $word2))[0];
+			$log_antonym{"$word1_synid-$word2_synid"} .= $log_antonym{"$word1_synid-$word2_synid"} ? "|$key_1-$key_2" : "$key_1-$key_2" unless $log_antonym{"$word1_synid-$word2_synid"} =~ /$key_1-$key_2/;
+			$log_antonym{"$word2_synid-$word1_synid"} .= $log_antonym{"$word2_synid-$word1_synid"} ? "|$key_2-$key_1" : "$key_2-$key_1" unless $log_antonym{"$word2_synid-$word1_synid"} =~ /$key_2-$key_1/;
+		    }
 		}
 	    }
 	}
-    }
 
-    if ($option{log} and $opt{log_merge}) {
-	close(LM);
-    }
+	if ($option{log} and $opt{log_merge}) {
+	    close(LM);
+	}
 
-    close(ANT);
+	close(ANT);
+    }
 }
 
 
@@ -350,7 +381,7 @@ if ($opt{convert_file}) {
 	foreach my $expression (@{$syn_group{$synid}}) {
 
 	    # タグを取る
-	    my $tag = $1 if $expression =~ s/\[(定義文|DIC|Web|Wikipedia|同義句)\]$//g;
+	    my $tag = $1 if $expression =~ s/\[(定義文|DIC|Web|Wikipedia|同義句|Med)\]$//g;
 
 	    # 定義文の重複を除く
 	    if ($tag eq '定義文') {
