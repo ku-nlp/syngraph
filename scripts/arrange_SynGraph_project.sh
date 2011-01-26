e1afd257a3c3051d5bd7bc9400555ce2f0fb9c0c
Tomohide Shibata <shibata@i.kyoto-u.ac.jp>
2011-01-26 08:39:39 +0000

    small change.

--- scripts/arrange_SynGraph_project.sh
+++ scripts/arrange_SynGraph_project.sh
@@ -4,7 +4,19 @@
 
 # usage: cd scripts; ./arrange_SynGraph_project.sh
 
-rm -fr ../syndb/x86_64
+uname=x86_64
+while getopts i OPT
+do  
+    case $OPT in
+        i)  uname=i686
+            ;;
+        h)  usage
+            ;;
+    esac
+done
+shift `expr $OPTIND - 1`
+
+rm -fr ../syndb/$uname
 rm -fr ../syndb/cgi
 rm -fr ../ExtractSynfromDic
 rm -fr ../MakeDic
