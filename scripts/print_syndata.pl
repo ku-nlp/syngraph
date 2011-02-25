5257b99089a343d25c30e56358ff1385cbff6ea4
Tomohide Shibata <shibata@i.kyoto-u.ac.jp>
2011-02-25 13:31:22 +0000

    storableをはくオプションを追加

--- scripts/print_syndata.pl
+++ scripts/print_syndata.pl
@@ -11,12 +11,27 @@ use SynGraph;
 use utf8;
 binmode STDIN, ':encoding(euc-jp)';
 binmode STDOUT, ':encoding(euc-jp)';
+use Getopt::Long;
+use Storable qw(nstore retrieve);
+
+my (%opt);
+GetOptions(\%opt, 'storable_db=s', 'read_storable_db=s', 'dbname=s');
 
 my $syndata_mldbm = $ARGV[0];
 
 my %db;
+if ($opt{storable_db}) {
+    &SynGraph::retrieve_mldbm($syndata_mldbm, \%db);
 
-&SynGraph::tie_mldbm($syndata_mldbm, \%db);
+    nstore(\%db, $opt{storable_db});
+}
+elsif ($opt{read_storable_db}) {
+    my $syndata = retrieve($opt{read_storable_db});
+
+    &SynGraph::store_mldbm($opt{dbname}, $syndata);
+}
+else {
+    &SynGraph::tie_mldbm($syndata_mldbm, \%db);
 
 # 出力例
 
@@ -24,17 +39,18 @@ my %db;
 #  オープン/おーぷん|オープンだ/おーぷんだ|s1703:オープン/おーぷん|s35492:オープン/おーぷん
 #  キャンパス/きゃんぱす|s31654:キャンパス/きゃんぱす
 
-foreach my $key ( sort { $a cmp $b } keys %db) {
-    print $key, "\n";
+    foreach my $key ( sort { $a cmp $b } keys %db) {
+	print $key, "\n";
 
-    # 基本句
-    foreach my $bp (@{$db{$key}}) {
-	my @ids;
+	# 基本句
+	foreach my $bp (@{$db{$key}}) {
+	    my @ids;
 
-	# 基本ノードとSynノード
-	foreach my $node (@{$bp->{nodes}}) {
-	    push @ids, $node->{id};
+	    # 基本ノードとSynノード
+	    foreach my $node (@{$bp->{nodes}}) {
+		push @ids, $node->{id};
+	    }
+	    print ' ', join('|', @ids), "\n";
 	}
-	print ' ', join('|', @ids), "\n";
     }
 }
